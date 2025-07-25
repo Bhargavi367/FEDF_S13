a584889b968029085c3819390d60d38bc70ba574
Bhargavi367 <bhargavikilaru595@gmail.com>
2025-07-25 13:49:27 +0530

    file created

--- DEV++/ee.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char greeting[] = "Hello";
-	printf("%s",greeting);
-	return 0;
-}
--- DEV++/haaa.c
+++
@@ -1,6 +0,0 @@
-struct studentbiodata
-{
-	char name_of_the_student[10];
-	int roll_number[10];
-	
-};
